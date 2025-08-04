72dd4bd779ffcb81b96286727a267e7d798d8218
AhmedRamzy0 <122788418+AhmedRamzy0@users.noreply.github.com>
2025-08-04 13:26:03 +0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,26 +1,15 @@
-module "network" {
-  source = "./network"
-  vpc-cidr = var.vpc-cidr
-  SAP_Private_Subnet_QAS_cidr = var.SAP_Private_Subnet_QAS_cidr
-  SAP_Public_2_cidr = var.SAP_Public_2_cidr
-  SAP_Private_Subnet_SOLMGR_cidr = var.SAP_Private_Subnet_SOLMGR_cidr
-  Firewall_Subnet_public_cidr = var.Firewall_Subnet_public_cidr
-  Firewall_private_Subnet = var.Firewall_private_Subnet
-  destination_cidrs = var.destination_cidrs
-  CR-SUB-11 =  var.CR-SUB-11
-  sap-dev-2 = var.sap-dev-2
-  sap-prv-dev = var.sap-prv-dev
-  sap-prv-prdapp = var.sap-prv-prdapp
-  sap-prv-prddb = var.sap-prv-prddb
+resource "aws_s3_bucket" "cr-bucket" {
+  bucket = var.Bucket_name
+
+  force_destroy     = true
+  object_lock_enabled = false
 }
 
+resource "aws_s3_bucket_public_access_block" "s3-dis-pubacc" {
+  bucket = aws_s3_bucket.cr-bucket.id
 
-
-module "S3" {
-  source = "./s3"
-  Bucket_name = var.Bucket_name
-  block_public_acls = var.block_public_acls
+  block_public_acls   = var.block_public_acls
   block_public_policy = var.block_public_policy
-  ignore_public_acls = var.ignore_public_acls
+  ignore_public_acls  = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
 }
--- variables.tf
+++ variables.tf
@@ -1,40 +1,29 @@
-variable "vpc-cidr" {
+variable "Bucket_name" {
+  description = "S3 Bucket Name"
   type = string
 }
 
 
-variable "SAP_Private_Subnet_QAS_cidr" {
-  type = string
+variable "block_public_acls" {
+  description = "Block public ACLs for the bucket"
+  type        = bool
+  default     = true
 }
 
-variable "SAP_Public_2_cidr" {
-  type = string
+variable "block_public_policy" {
+  description = "Block public policies for the bucket"
+  type        = bool
+  default     = true
 }
 
-variable "SAP_Private_Subnet_SOLMGR_cidr" {
-  type = string
+variable "ignore_public_acls" {
+  description = "Ignore public ACLs for the bucket"
+  type        = bool
+  default     = true
 }
 
-variable "Firewall_Subnet_public_cidr" {
-  type = string
+variable "restrict_public_buckets" {
+  description = "Restrict public buckets"
+  type        = bool
+  default     = true
 }
-
-
-variable "Firewall_private_Subnet" {
-  type = string
-}
-
-
-
-
-variable "destination_cidrs" {
-  type    = list(string)
-  default = ["0.0.0.0/0", "172.16.16.0/21"]  # local to FW CIDR blocks
-}
-
-variable "CR-SUB-11" {}
-variable "sap-dev-2" {}
-variable "sap-prv-dev" {}
-variable "sap-prv-prddb" {}
-variable "sap-prv-prdapp" {}
-
